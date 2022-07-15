5ef30ac341d4bd27863025290ea144854bbe13d4
ttasjwi <ttasjwi920@gmail.com>
2022-07-15 19:26:49 +0900

    [예제 04-16] 네이밍 컨벤션

---
+++ 04장. 변수/04-16.js
@@ -0,0 +1,14 @@
+
+// 카멜 케이스 (camelCase) : 주로 변수, 함수명에 사용
+var firstName;
+
+// 스네이크 케이스 (snakeCase)
+var first_name;
+
+// 파스칼 케이스 (PascalCase) : 주로 생성자 함수, 클래스 이름에 사용
+var FirstName;
+
+// 헝가리언 케이스(typeHungarianCase)
+var strFirstName;
+var $elem = document.getElementById('myId');
+var observable$ = fromEvent(document, 'click'); // RxJs 옵저버블
