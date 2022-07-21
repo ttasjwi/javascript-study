41c7449fc1cd9a0f522c273ed5da92162bba737e
ttasjwi <ttasjwi920@gmail.com>
2022-07-21 16:06:36 +0900

    [예제 06-04] 숫자 타입의 세 가지 특별한 값

---
+++ 06장. 데이터 타입/06-04.js
@@ -0,0 +1,9 @@
+
+// 숫자타입의 세가지 특별한 값
+console.log(10/0); // Infinity : 양의 무한대
+console.log(10/-0); // Infinity : 양의 무한대
+console.log(1 * 'String'); // NaN : Not a Number  (산술연산 불가, 숫자가 아님)
+
+console.log(Infinity);
+console.log(-Infinity);
+console.log(NaN);
