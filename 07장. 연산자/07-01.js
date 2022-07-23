ce6834cb70efea6225c6f66ebcbb63a858806fff
ttasjwi <ttasjwi920@gmail.com>
2022-07-23 23:39:53 +0900

    [예제 07-01] 하나 이상의 표현식을 대상으로 연산을 수행해 값을 만들 수 있다
    - 연산자 : 피연산자를 대상으로 '연산'하여 새로운 값 생성
    - 피연산자 : 연산의 대상이 되는 표현식

---
+++ 07장. 연산자/07-01.js
@@ -0,0 +1,20 @@
+// 산술 연산자
+console.log(5 * 4); // 20
+
+// 문자열 연결 결합자
+console.log('My name is ' + 'Lee'); //'My name is Lee'
+
+// 할당 연산자 -> 'red'
+let color = 'red';
+console.log(color);
+
+// 비교 연산자
+let bool = 3 > 5;
+console.log(bool);
+
+// 논리 연산자
+let bool2 = true && false;
+console.log(bool2);
+
+// 타입 연산자
+console.log(typeof 'Hi'); // String
